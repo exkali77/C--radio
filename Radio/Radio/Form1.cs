a566beee8d9a3409b381215cbccb042297c68fb4
Kerem Şenses <68199514+exkali77@users.noreply.github.com>
2023-10-29 11:24:46 +0300

    C# radio
    C# radio programı

---
+++ Radio/Radio/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Radio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            //radio linkini internetten bulup aşağı yapıştırın
+            axWindowsMediaPlayer1.URL = @"http://95.173.188.155:9994/listen.pls";//mevcut link çalışmıyor
+        }
+    }
+}
